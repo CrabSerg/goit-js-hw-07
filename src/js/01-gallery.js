67e026e4c731fc168ba74a9ed1a5f95f286b10f2
Serg <serg212007@gmail.com>
2022-10-17 21:43:27 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+let base = prompt("Напиши какое-то число");
+base = Number(base);
+console.log(base);
+
+let power = prompt("В сколько умножить?");
+power = Number(power);
+console.log(power);
+
+console.log("Будет число:", base*power)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
+
+
