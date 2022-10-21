040b816888a0e2d88935f300083f125c1bdb0e26
Serg <serg212007@gmail.com>
2022-10-21 12:26:39 +0300

    12334

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,98 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galeryRef = document.querySelector(`.gallery`)
 
-const galeryMakrup = galleryItems.map((element)=>
-`<div class="gallery__item">
-    <a class="gallery__link" href="${element.original}">
-        <img
-            class="gallery__image"
-            src="${element.preview}"
-            data-source="${element.original}"
-            alt="${element.description}"
-        />
-    </a>
-</div>`
-)
-.join("");
+/* -------------------------------------------------------------------------- */
+/*                                     new                                    */
+/* -------------------------------------------------------------------------- */
 
+const galleryRef = document.querySelector('.gallery')
+const galleryCreate = makeGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryCreate);
+galleryRef.addEventListener('click', galleryModal);
 
-galeryRef.insertAdjacentHTML("afterbegin", galeryMakrup);
+// створюємо елементи HTML 
+function makeGallery(galleryItems) {
+  return galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+    </div>`;
+  })
+  .join("");
+}
 
+// створюємо елементи галереї
+const instance = basicLightbox.create(`<img src="">`, {
+  onShow: () => { document.addEventListener("keydown", onEsc) }, // опція-функція basicLightbox, додаємо подію при появі модалки, для кнопки 
+  onClose: () => { document.removeEventListener("keydown", onEsc) }// опція-функція basicLightbox, прибираємо подію після натискання Escape
+});
 
-const instance = Object.create
-basicLightbox.create(`
-    <img src="${element.original}" width="800" height="600">
-`)
+// 
+function galleryModal(event){
+  event.preventDefault(); // забороняємо відкривати посилання/фото в новому вікні
+  if (event.target.nodeName !== "IMG") return; // якщо клік не на картинці нічого не робимо
+  
+  // при відкритті модалки атрибут src всім елементам img змінюємо на source
+  instance.element().querySelector("img").src = event.target.dataset.source; 
+  
+  instance.show(); // викликаємо вбудовану функцію для відображення модалки
+};
 
-instance.show()
+// функція для події натискання на кнопку
+function onEsc(event) {
+     if (event.code === "Escape") {
+      instance.close();
+    }
+  }
 
+/* -------------------------------------------------------------------------- */
+/*                                     old                                    */
+/* -------------------------------------------------------------------------- */
 
-console.log(instance)
+// const galleryRef = document.querySelector(".gallery")
 
+// // створення колекції елементів з масиву galleryItems
+// galleryRef.innerHTML = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<div class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+//       </a>
+//     </div>`
+//   })
+//   .join("");
+
+// // функція створення модального вікна з допомогою бібліотеки basicLightbox
+// // function galleryModal(event){
+// //   event.preventDefault(); // забороняємо відкривати посилання/фото в новому вікні
+// //   if (event.target.nodeName !== "IMG") return; // якщо клік не на картинці нічого не робимо
+
+// //   const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`); // створюємо модальне вікно 
+
+// //   instance.show(); // викликаємо вбудовану функцію для відображення модалки
+// // };
+
+// // ============= функція з врахуванням кнопки виходу Escape
+
+// function galleryModal(event){
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") return;
+
+//   const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`, {
+//     onShow: () => { window.addEventListener("keydown", onEsc) }, // опція-функція basicLightbox, додаємо подію при появі модалки, для кнопки 
+//     onClose: () => { window.removeEventListener("keydown", onEsc) }// опція-функція basicLightbox, прибираємо подію після натискання Escape
+//   });
+//   // функція перевірки чи дійсно натиснула кнопка, повертає закриття модалки
+//   function onEsc(event) {
+//      if (event.code === "Escape") {
+//       instance.close();//
+//     }
+//   }
+//   instance.show();
+// };
+
+// galleryRef.addEventListener("click", galleryModal);
+
+// // console.log();
